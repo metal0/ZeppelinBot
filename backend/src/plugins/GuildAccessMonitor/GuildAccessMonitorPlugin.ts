6c67be42857f0e98a7899fd4e0a0b153ca9a61ad
metal0 <metal@i0.tf>
2021-08-06 14:13:48 +0100

    add back guild.leave
    Signed-off-by: metal0 <metal@i0.tf>

--- backend/src/plugins/GuildAccessMonitor/GuildAccessMonitorPlugin.ts
+++ backend/src/plugins/GuildAccessMonitor/GuildAccessMonitorPlugin.ts
@@ -14,8 +14,8 @@ interface GuildAccessMonitorPluginType extends BasePluginType {
 async function checkGuild(pluginData: GlobalPluginData<GuildAccessMonitorPluginType>, guild: Guild) {
   if (!(await pluginData.state.allowedGuilds.isAllowed(guild.id))) {
     console.log(`Non-allowed server ${guild.name} (${guild.id}), leaving`);
-    console.log("[Temporarily not leaving automatically]");
-    // guild.leave();
+    // console.log("[Temporarily not leaving automatically]");
+    guild.leave();
   }
 }
 
