29d79a271cee70ec95f6611a4f8d2f39ccf11461
metal <admin@metalruller.com>
2021-08-16 16:59:11 +0000

    zep_check

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -438,7 +438,7 @@ export function convertMSToDelayString(ms: number): string {
   return result;
 }
 
-export function successMessage(str, emoji = "<:zep_check:650361014180904971>") {
+export function successMessage(str, emoji = "✅") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
