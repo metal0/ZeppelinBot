c2a2cafe8ba76798c433ce38180c76a8f2d7f5c7
almeidx <almeidx@pm.me>
2021-08-29 16:13:00 +0100

    added thread_create_spam automod trigger

--- backend/src/plugins/Automod/events/runAutomodOnThreadEvents.ts
+++ backend/src/plugins/Automod/events/runAutomodOnThreadEvents.ts
@@ -1,4 +1,5 @@
 import { typedGuildEventListener } from "knub";
+import { RecentActionType } from "../constants";
 import { runAutomod } from "../functions/runAutomod";
 import { AutomodContext, AutomodPluginType } from "../types";
 
@@ -16,6 +17,13 @@ export const RunAutomodOnThreadCreate = typedGuildEventListener<AutomodPluginTyp
     };
 
     pluginData.state.queue.add(() => {
+      pluginData.state.recentActions.push({
+        type: RecentActionType.ThreadCreate,
+        context,
+        count: 1,
+        identifier: null,
+      });
+
       runAutomod(pluginData, context);
     });
   },
