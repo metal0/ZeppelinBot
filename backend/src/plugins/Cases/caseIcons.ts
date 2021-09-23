7d4c0649871e0cfadc5a67a86cdba70af843471b
metal <admin@metalruller.com>
2021-09-23 09:54:42 +0000

    case emojis

--- backend/src/plugins/Cases/caseIcons.ts
+++ backend/src/plugins/Cases/caseIcons.ts
@@ -1,13 +1,13 @@
 import { CaseTypes } from "../../data/CaseTypes";
 
 export const caseIcons: Record<CaseTypes, string> = {
-  [CaseTypes.Ban]: "<:case_ban:742540201443721317>",
-  [CaseTypes.Unban]: "<:case_unban:742540201670475846>",
-  [CaseTypes.Note]: "<:case_note:742540201368485950>",
-  [CaseTypes.Warn]: "<:case_warn:742540201624338454>",
-  [CaseTypes.Kick]: "<:case_kick:742540201661825165>",
-  [CaseTypes.Mute]: "<:case_mute:742540201817145364>",
-  [CaseTypes.Unmute]: "<:case_unmute:742540201489858643>",
-  [CaseTypes.Deleted]: "<:case_deleted:742540201473343529>",
-  [CaseTypes.Softban]: "<:case_softban:742540201766813747>",
+  [CaseTypes.Ban]: "<:case_ban:890535999006986281>",
+  [CaseTypes.Unban]: "<:case_unban:890535999006990416>",
+  [CaseTypes.Note]: "<:case_note:890535999162179604>",
+  [CaseTypes.Warn]: "<:case_warn:890535998780489749>",
+  [CaseTypes.Kick]: "<:case_kick:890535999149572096>",
+  [CaseTypes.Mute]: "<:case_mute:890535998826635265>",
+  [CaseTypes.Unmute]: "<:case_unmute:890535998642077707>",
+  [CaseTypes.Deleted]: "<:case_deleted:890535998809849867>",
+  [CaseTypes.Softban]: "<:case_softban:890535998616903692>",
 };
