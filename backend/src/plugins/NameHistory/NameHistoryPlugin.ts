75b02acf88070c774b73e04fa0465e33229b0e79
metal <admin@metalruller.com>
2021-12-30 10:03:42 +0000

    re-enable namehistory

--- backend/src/plugins/NameHistory/NameHistoryPlugin.ts
+++ backend/src/plugins/NameHistory/NameHistoryPlugin.ts
@@ -39,9 +39,8 @@ export const NameHistoryPlugin = zeppelinGuildPlugin<NameHistoryPluginType>()({
 
   // prettier-ignore
   events: [
-    // FIXME: Temporary
-    // ChannelJoinEvt,
-    // MessageCreateEvt,
+    ChannelJoinEvt,
+    MessageCreateEvt,
   ],
 
   beforeLoad(pluginData) {
